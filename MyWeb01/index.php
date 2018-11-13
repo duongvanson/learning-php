b924b6be858bdf2b6956f65a48a09aa55b33cfea
Duong Son <duongvansonit@gmail.com>
2018-11-13 08:44:53 +0700

    html

---
+++ MyWeb01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>MyWeb</title>
+</head>
+<body>
+	<header>
+		<h1 class="logo">VSond Web Developer</h1>
+	</header>
+	<div id="main">
+		<div id="side-l">
+			
+		</div>
+	</div>
+	<footer>
+		<h2 class="logo">Copyright &copy; VSond</h2>
+	</footer>
+</body>
+</html>
